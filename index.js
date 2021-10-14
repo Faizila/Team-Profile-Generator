add2151b9863b1f48b28d80c44a41235af893a48
Faizila Amlani <amlani.faizila@gmail.com>
2021-10-14 16:57:33 -0500

    added const and linked all js files in index.js

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+// node modules 
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+// link to page creation
+const generateHTML = require(' ');
+
+// team profiles
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+
+// team array
+const teamArray = [];
 
